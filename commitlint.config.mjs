ab955da6d8ef372690e0214dfdb409c52a4c646e
Maengdok <maengdok@outlook.com>
2026-05-06 13:28:24 +0200

    chore: update husky error message, display an example of expected commit

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,3 +1,18 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  helpUrl:
+    'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+  prompt: {
+    messages: {
+      skip: '(press enter to skip)',
+      max: 'upper %d chars',
+      min: '%d chars at least',
+      emptyWarning: 'can not be empty',
+      upperLimitWarning: 'over limit',
+      lowerLimitWarning: 'below limit',
+    },
+  },
+  rules: {
+    'header-max-length': [2, 'always', 72],
+  },
 };
