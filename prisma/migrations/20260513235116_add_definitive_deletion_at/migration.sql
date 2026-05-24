21d2ac1277ea34bb525697020c5391303fd430f0
Axel <maengdok@outlook.com>
2026-05-24 13:56:09 +0200

    Revert "test(auth): add unit tests and reorganize auth module structure"

--- prisma/migrations/20260513235116_add_definitive_deletion_at/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "Campaign" ADD COLUMN     "definitiveDeletionAt" TIMESTAMP(3);
-
--- CreateIndex
-CREATE INDEX "Campaign_definitiveDeletionAt_idx" ON "Campaign"("definitiveDeletionAt");
